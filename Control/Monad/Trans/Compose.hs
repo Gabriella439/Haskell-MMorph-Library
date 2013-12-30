eac6395c966a253ebc9f0c967f78a1644e2d0a77
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-12-30 11:11:08 -0800

    Polished @duairc's work in `Control.Monad.Trans.Compose`

--- Control/Monad/Trans/Compose.hs
+++ Control/Monad/Trans/Compose.hs
@@ -6,20 +6,21 @@
 -}
 
 module Control.Monad.Trans.Compose (
+    -- * ComposeT
     ComposeT(ComposeT, getComposeT),
    ) where
 
-import Control.Applicative (Applicative, pure, (<*>), (*>), (<*), Alternative, empty, (<|>))
-import Control.Monad (MonadPlus, mzero, mplus, liftM)
-import Control.Monad.Morph (MFunctor, hoist)
-import Control.Monad.Trans.Class (MonadTrans, lift)
-import Control.Monad.IO.Class (MonadIO, liftIO)
-import Data.Foldable (Foldable, fold, foldMap, foldr, foldr', foldl', foldl, foldr1, foldl1)
-import Data.Traversable (Traversable, traverse, sequenceA, mapM, sequence)
+import Control.Applicative (
+    Applicative(pure, (<*>), (*>), (<*)), Alternative(empty, (<|>)) )
+import Control.Monad (MonadPlus(mzero, mplus), liftM)
+import Control.Monad.Morph (MFunctor(hoist))
+import Control.Monad.Trans.Class (MonadTrans(lift))
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Data.Foldable (Foldable(fold, foldMap, foldr, foldl, foldr1, foldl1))
+import Data.Traversable (Traversable(traverse, sequenceA, mapM, sequence))
 import Prelude hiding (foldr, foldl, foldr1, foldl1, mapM, sequence)
 
-{- | Right-to-left composition of monad transformers.
--}
+-- | Composition of monad transformers.
 newtype ComposeT (f :: (* -> *) -> * -> *) (g :: (* -> *) -> * -> *) m a
     = ComposeT { getComposeT :: f (g m) a }
 
@@ -42,8 +43,8 @@ instance Alternative (f (g m)) => Alternative (ComposeT f g m) where
 
 instance Monad (f (g m)) => Monad (ComposeT f g m) where
     return a = ComposeT (return a)
-    ComposeT m >>= f = let f' a = let ComposeT m' = f a in m' in ComposeT (m >>= f')
-    fail e = ComposeT (fail e)
+    m >>= f  = ComposeT (getComposeT m >>= \x -> getComposeT (f x))
+    fail e   = ComposeT (fail e)
 
 instance MonadPlus (f (g m)) => MonadPlus (ComposeT f g m) where
     mzero = ComposeT mzero
@@ -53,17 +54,15 @@ instance MonadIO (f (g m)) => MonadIO (ComposeT f g m) where
     liftIO m = ComposeT (liftIO m)
 
 instance Foldable (f (g m)) => Foldable (ComposeT f g m) where
-    fold (ComposeT m) = fold m
-    foldMap f (ComposeT m) = foldMap f m
-    foldr f a (ComposeT m) = foldr f a m
-    foldr' f a (ComposeT m) = foldr' f a m
-    foldl f a (ComposeT m) = foldl f a m
-    foldl' f a (ComposeT m) = foldl' f a m
-    foldr1 f (ComposeT m) = foldr1 f m
-    foldl1 f (ComposeT m) = foldl1 f m
+    fold        (ComposeT m) = fold m
+    foldMap f   (ComposeT m) = foldMap f   m
+    foldr   f a (ComposeT m) = foldr   f a m
+    foldl   f a (ComposeT m) = foldl   f a m
+    foldr1 f    (ComposeT m) = foldr1  f   m
+    foldl1 f    (ComposeT m) = foldl1  f   m
 
 instance Traversable (f (g m)) => Traversable (ComposeT f g m) where
-    traverse f (ComposeT m) = fmap ComposeT (traverse f m)
-    sequenceA (ComposeT m) = fmap ComposeT (sequenceA m)
-    mapM f (ComposeT m) = liftM ComposeT (mapM f m)
-    sequence (ComposeT m) = liftM ComposeT (sequence m)
+    traverse f (ComposeT m) = fmap  ComposeT (traverse f m)
+    sequenceA  (ComposeT m) = fmap  ComposeT (sequenceA  m)
+    mapM     f (ComposeT m) = liftM ComposeT (mapM     f m)
+    sequence   (ComposeT m) = liftM ComposeT (sequence   m)
